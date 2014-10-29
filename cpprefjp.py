9084bd4fb81b098d8ad29cc6e81d6dec9f56d3a1
melpon <melt@e-ml.jp>
2014-10-30 02:24:37 +0900

    cpprefjp と boostjp を共有で使えるように設定

---
+++ cpprefjp.py
@@ -0,0 +1,14 @@
+#coding: utf-8
+
+# 入力ディレクトリ
+INPUT_DIR = 'cpprefjp.site'
+
+# 出力ディレクトリ
+OUTPUT_DIR = 'cpprefjp.github.io'
+
+# URL ベース
+BASE_URL = 'http://cpprefjp.github.io'
+EDIT_URL_FORMAT = 'https://github.com/cpprefjp/site/edit/master/{paths}'
+
+# テンプレートファイル
+PAGE_TEMPLATE = 'cpprefjp.template.html'
--- run.py
+++ run.py
@@ -1,32 +1,31 @@
+#!/usr/bin/env python
 #coding: utf-8
 import os
 import re
 import markdown
+import sys
+import importlib
 
-# 入力ディレクトリ
-INPUT_DIR = 'site'
+if len(sys.argv) != 2:
+    print '{} <setting>'.format(sys.argv[0])
+    sys.exit(0)
 
-# 出力ディレクトリ
-OUTPUT_DIR = 'cpprefjp.github.io'
-
-# URL ベース
-BASE_URL = 'http://cpprefjp.github.io'
-EDIT_URL_FORMAT = 'https://github.com/cpprefjp/site/edit/master/{paths}'
+settings = importlib.import_module(sys.argv[1])
 
 
 def convert(path, tmpl):
-    md_data = open(os.path.join(INPUT_DIR, path + '.md')).read()
+    md_data = open(os.path.join(settings.INPUT_DIR, path + '.md')).read()
     paths = path.split('/')
 
     qualified_fenced_code = 'markdown_to_html.qualified_fenced_code'
     html_attribute = 'markdown_to_html.html_attribute(base_url={base_url}, base_path={base_path}, full_path={full_path}, extension={extension})'.format(
-        base_url=BASE_URL,
+        base_url=settings.BASE_URL,
         base_path='/'.join(paths[:-1]),
         full_path=path + '.md',
         extension='.html',
     )
     # footer = 'markdown_to_html.footer(url={url})'.format(
-    #     url=EDIT_URL_FORMAT.format(
+    #     url=settings.EDIT_URL_FORMAT.format(
     #         paths=path + '.md',
     #     )
     # )
@@ -38,28 +37,28 @@ def convert(path, tmpl):
         html_attribute])
     html_data = md.convert(unicode(md_data, encoding='utf-8'))
 
-    dst_dir = os.path.dirname(os.path.join(OUTPUT_DIR, path))
+    dst_dir = os.path.dirname(os.path.join(settings.OUTPUT_DIR, path))
     if not os.path.exists(dst_dir):
         os.makedirs(dst_dir)
-    open(os.path.join(OUTPUT_DIR, path + '.html'), 'w').write(tmpl.replace('@@body@@', html_data.encode('utf-8')))
+    open(os.path.join(settings.OUTPUT_DIR, path + '.html'), 'w').write(tmpl.replace('@@body@@', html_data.encode('utf-8')))
 
 
 def target_paths():
-    for root, dirs, files in os.walk(INPUT_DIR):
+    for root, dirs, files in os.walk(settings.INPUT_DIR):
         for f in files:
             if f.endswith('.md'):
-                path = os.path.join(root, f)[len(INPUT_DIR):].lstrip('/')
+                path = os.path.join(root, f)[len(settings.INPUT_DIR):].lstrip('/')
                 # 更新対象でないファイルは無視する
                 if re.match('^([A-Z].*)|(.*!(\.md))$', path.split('/')[-1]):
                     continue
                 # .md を取り除く
-                path = path[:-3];
+                path = path[:-3]
                 yield path
 
 
 def make_pageinfo(path):
     paths = path.split('/')
-    title = paths[-1] # TODO
+    title = paths[-1]  # TODO
     filename = paths[-1]
     dirnames = paths[:-1]
     return {
@@ -79,7 +78,7 @@ def make_header(pageinfos):
 def main():
     pageinfos = [make_pageinfo(path) for path in target_paths()]
     header = make_header(pageinfos)
-    tmpl = open('template.html').read()
+    tmpl = open(settings.PAGE_TEMPLATE).read()
     tmpl = tmpl.replace('@@header@@', header)
     for pageinfo in pageinfos:
         print(pageinfo['path'])
