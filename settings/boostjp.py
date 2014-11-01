be062f39baf6037d91a2867b4f4ee5a4bf19f144
melpon <melt@e-ml.jp>
2014-11-01 20:20:24 +0900

    フォルダ分け

--- settings/boostjp.py
+++ settings/boostjp.py
@@ -1,14 +1,14 @@
 #coding: utf-8
 
 # 入力ディレクトリ
-INPUT_DIR = 'boostjp.site'
+INPUT_DIR = 'boostjp/site'
 
 # 出力ディレクトリ
-OUTPUT_DIR = 'boostjp.github.io'
+OUTPUT_DIR = 'boostjp/boostjp.github.io'
 
 # URL ベース
 BASE_URL = 'http://boostjp.github.io'
 EDIT_URL_FORMAT = 'https://github.com/boostjp/site/edit/master/{paths}'
 
 # テンプレートファイル
-PAGE_TEMPLATE = 'boostjp.template.html'
+PAGE_TEMPLATE = 'boostjp/template.html'
--- settings/cpprefjp.py
+++ settings/cpprefjp.py
@@ -1,14 +1,14 @@
 #coding: utf-8
 
 # 入力ディレクトリ
-INPUT_DIR = 'cpprefjp.site'
+INPUT_DIR = 'cpprefjp/site'
 
 # 出力ディレクトリ
-OUTPUT_DIR = 'cpprefjp.github.io'
+OUTPUT_DIR = 'cpprefjp/cpprefjp.github.io'
 
 # URL ベース
 BASE_URL = 'http://cpprefjp.github.io'
 EDIT_URL_FORMAT = 'https://github.com/cpprefjp/site/edit/master/{paths}'
 
 # テンプレートファイル
-PAGE_TEMPLATE = 'cpprefjp.template.html'
+PAGE_TEMPLATE = 'cpprefjp/template.html'
