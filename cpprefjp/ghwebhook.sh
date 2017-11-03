5a34b6dccd231f6398f45900134165a1a9eb7022
melpon <shigemasa7watanabe+github@gmail.com>
2017-11-03 12:23:08 +0900

    git pull 失敗時にうまく動かなくなっていたので修正
    docker 上でいろいろ実行するので、root ユーザのファイルが残ってしまい、
    自身のユーザで rm -rf しても消えなくなっていた。
    リポジトリを取得する部分を関数化して、docker 経由で rm -rf
    するようにした。

--- cpprefjp/ghwebhook.sh
+++ cpprefjp/ghwebhook.sh
@@ -5,54 +5,5 @@ set -e
 su - melpon -c "
 set -e
 cd site_generator
-git pull
-git submodule update -i
-
-rm -rf cpprefjp/static/static/kunai || true
-mkdir -p cpprefjp/static/static/kunai
-rm -rf cpprefjp/static/static/crsearch || true
-mkdir -p cpprefjp/static/static/crsearch
-
-export DOCKER_IT=\"\"
-
-# kunai 用 JS, CSS 生成
-if [ ! -d kunai ]; then
-  git clone git@github.com:cpprefjp/kunai.git
-else
-  pushd kunai
-  git pull || (cd .. && rm -rf kunai && git clone git@github.com:cpprefjp/kunai.git)
-  popd
-fi
-./kunai/docker.sh build
-./kunai/docker.sh install
-cp -r ./kunai/dist/* ./cpprefjp/static/static/kunai/
-
-# crsearch.json 生成
-if [ ! -d crsearch.json/site ]; then
-  git clone git@github.com:cpprefjp/site.git crsearch.json/site
-else
-  pushd crsearch.json/site
-  git pull || (cd .. && rm -rf site && git clone git@github.com:cpprefjp/site.git)
-  popd
-fi
-./crsearch.json/docker.sh build
-./crsearch.json/docker.sh run
-cp crsearch.json/crsearch.json cpprefjp/static/static/crsearch/
-
-# サイト生成
-if [ ! -d cpprefjp/site ]; then
-  git clone git@github.com:cpprefjp/site.git cpprefjp/site
-else
-  pushd cpprefjp/site
-  git pull || (cd .. && rm -rf site && git clone git@github.com:cpprefjp/site.git)
-  popd
-fi
-
-./docker.sh build
-./docker.sh run settings.cpprefjp \"$@\"
-
-cd cpprefjp/cpprefjp.github.io
-git add ./ --all
-git commit -a \"--author=cpprefjp-autoupdate <shigemasa7watanabe@gmail.com>\" -m \"update automatically\"
-git push origin master 2>/dev/null
+cpprefjp/ghwebhook_on_user.sh \"$@\"
 "
---
+++ cpprefjp/ghwebhook_on_user.sh
@@ -0,0 +1,54 @@
+#!/bin/bash
+
+set -e
+git pull
+git submodule update -i
+
+rm -rf cpprefjp/static/static/kunai || true
+mkdir -p cpprefjp/static/static/kunai
+rm -rf cpprefjp/static/static/crsearch || true
+mkdir -p cpprefjp/static/static/crsearch
+
+export DOCKER_IT=""
+
+function rm_rf() {
+  docker run -v `pwd`:/var/src alpine:3.6 /bin/sh -c "rm -rf /var/src/$1 || true"
+}
+function clone_and_fallback() {
+  if [ ! -d $1/.git ]; then
+    rm_rf $1
+    git clone $2 $1
+  else
+    pushd $1
+    if git pull; then
+      popd
+    else
+      popd
+      rm_rf $2
+      git clone $2 $1
+    fi
+  fi
+}
+
+# kunai 用 JS, CSS 生成
+clone_and_fallback kunai git@github.com:cpprefjp/kunai.git
+./kunai/docker.sh build
+./kunai/docker.sh install
+cp -r ./kunai/dist/* ./cpprefjp/static/static/kunai/
+
+# crsearch.json 生成
+clone_and_fallback crsearch.json/site git@github.com:cpprefjp/site.git
+./crsearch.json/docker.sh build
+./crsearch.json/docker.sh run
+cp crsearch.json/crsearch.json cpprefjp/static/static/crsearch/
+
+# サイト生成
+clone_and_fallback cpprefjp/cpprefjp.github.io git@github.com:cpprefjp/cpprefjp.github.io.git
+clone_and_fallback cpprefjp/site git@github.com:cpprefjp/site.git
+./docker.sh build
+./docker.sh run settings.cpprefjp "$@"
+
+cd cpprefjp/cpprefjp.github.io
+git add ./ --all
+git commit -a "--author=cpprefjp-autoupdate <shigemasa7watanabe@gmail.com>" -m "update automatically"
+git push origin master 2>/dev/null
